0e8575d12e5acb1c7889136d30e83ca4961bad8c
narae830 <68041257+narae830@users.noreply.github.com>
2022-10-06 02:15:28 +0900

    faq

--- assets/js/common.js
+++ assets/js/common.js
@@ -48,7 +48,6 @@ function modalResize() {
             });
         })
     }
-
     function setTabScroll() {
         $('.tab-type1').each(function(){
             var $this = $(this);
@@ -56,6 +55,27 @@ function modalResize() {
             var moveScroll = tabList.children('li.on').position().left - tabList.css('padding-left').replace(/[^-\d\.]/g, '');
             tabList.scrollLeft(moveScroll)
         })
+    }
+    function accToggle() {
+        $('.faq-list').each(function(){
+            var $this = $(this);
+            var wrap = $this.find('.acc-head');
+            wrap.on('click',function(e){
+                e.preventDefault();
+                var $this = $(this);
+                var val = $this.attr('aria-expanded');
+                var con = $this.next('.acc-cont');
+                if(val != 'true'){
+                    $this.attr('aria-expanded','true');
+                    con.hide();
+                    con.stop().slideDown();
+                }else{
+                    con.stop().slideUp(function(){
+                        $this.attr('aria-expanded','false');
+                    });
+                }
+            });
+        });
     }
     function gnbMenu() {
         $('.gnb').css({left: '-100%'});
@@ -76,6 +96,7 @@ function modalResize() {
             modalResize();
             setTabScroll();
             tabToggle();
+            accToggle()
             gnbMenu();
         },
         "resize":function(){
